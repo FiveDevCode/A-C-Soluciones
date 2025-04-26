161c0667c6f2707b6d5b7f108fd922b6940073a8
David Ortiz <dao.rootdev@gmail.com>
2025-04-26 02:59:42 -0500

    implementación de Sequelize para cliente.model

--- Backend/src/database/conexion.js
+++ Backend/src/database/conexion.js
@@ -1,23 +1,26 @@
+import Sequelize from 'sequelize'
 import 'dotenv/config';
-import pkg from 'pg';
-
-const { Pool } = pkg ;
 
 
 // configuramos la conexion con la base de datos postgresql
-export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+export const sequelize = new Sequelize( process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false
 });
+
 
 // conexion con la base de datos 
 export const conectDB = async () => {
     try {
-        const demoProyect = await pool.connect();
-        console.log('Conectado a la base de datos');
-        demoProyect.release();
+        await sequelize.authenticate ();
+        console.log('Conectado a la base de datos PostgreSQL con Sequelize');
+        await sequelize.sync({alter: true})
+        console.log("Tablas sincronizadas");
     } catch (error) {
         console.log('Error a conectarse a la base de datos', error);
         process.exit(1);
     }
     
 };
+
+
