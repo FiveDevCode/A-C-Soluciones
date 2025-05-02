c206cdae656662be5ed02219c42fc21270cdb0a4
Victor <victor.raffan@correounivalle.edu.co>
2025-05-01 19:21:02 -0500

    pantallas de generar reportes y servicio tecnico

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PerfilUsuario from './components/tecnico/PerfilUsuario';
+import GenerateReportsTc from './components/tecnico/GenerateReportsTc';
+import { colors } from '@mui/material';
+import ServiceTc from './components/tecnico/ServiceTc';
 
-// Puedes crear un componente de Login simple por ahora
+
 function Login() {
   return (
     <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
@@ -11,12 +14,42 @@ function Login() {
   );
 }
 
+function Report(){
+  return(
+    
+        <div>
+          <h2 style={{ textAlign: 'center' }}>Formulario de Reporte</h2>
+          <GenerateReportsTc />
+        </div>
+     
+    
+  );
+}
+
+
+
+
+const tareaDemo = {
+  nombre: 'Revisión de válvulas de presión en planta sur',
+  descripcion: `Realizar inspección preventiva y mantenimiento de las válvulas de presión ubicadas
+en la planta sur. Verificar estado de sellos, realizar limpieza, lubricación y
+reemplazo si es necesario. Documentar hallazgos y tomar fotografías del antes y después.`,
+  fechaLimite: '25/04/2025',
+  estado: 'pendiente'
+};
+
+{/*<Route path="/detalle-tarea" element={<DetalleTarea tarea={tareaDemo} />} />*/}
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<PerfilUsuario />} />
-        <Route path="/login" element={<Login />} />
+       {/* <Route path="/" element={<PerfilUsuario />} />
+        <Route path="/login" element={<Login />} /> 
+        <Route path='/' element={<GenerateReportsTc/>}/>
+        <Route path='/Report' element={<Report/>}/>*/}
+        
+        <Route path='/' element={<ServiceTc tarea={tareaDemo}/>}/>
       </Routes>
     </Router>
   );
