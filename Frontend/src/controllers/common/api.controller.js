b7a9686bdabc30106a9bbce60bd03dad307f6850
jonier255 <jonier145@gmail.com>
2025-07-10 00:39:06 -0500

    Implementacion de la ruta para el acceso en una variable de entorno

--- Frontend/src/controllers/common/api.controller.js
+++ Frontend/src/controllers/common/api.controller.js
@@ -1,6 +1,8 @@
 // src/services/api.js
 import axios from 'axios';
 
+const API_KEY = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
   baseURL: `${API_KEY}/api`, // puedes usar una variable de entorno
   
