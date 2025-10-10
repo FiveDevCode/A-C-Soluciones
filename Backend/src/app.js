b4f8dfbb351e9cedb9a25aa728779e4dfbd40bd7
Luis Mateus Tegue Torres <112036741+MateusTegue@users.noreply.github.com>
2025-10-10 02:18:10 -0500

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -41,7 +41,6 @@ App.use(cors({
  origin: function (origin, callback) {
     const allowedOrigins = [
      'https://a-c-soluciones.vercel.app',
-      '*',
       'http://localhost:3000',
       'http://127.0.0.1:3000'
     ];
