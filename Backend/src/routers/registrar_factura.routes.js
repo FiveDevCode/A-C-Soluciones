15dc320b806bc6c36dae405740772b1d19153997
jonier255 <jonier145@gmail.com>
2025-10-14 12:15:05 -0500

    probando cambios exitosos de Daniel

--- Backend/src/routers/registrar_factura.routes.js
+++ Backend/src/routers/registrar_factura.routes.js
@@ -1,22 +1,22 @@
 import { RegistrarFacturasController } from '../controllers/registrar_facturas.controller.js';
 import { Router } from 'express';
-import { authenticate, isAdminOrContabilidad} from '../middlewares/autenticacion.js';
+import { authenticate, isAdminOrContador} from '../middlewares/autenticacion.js';
 
 const router = Router();
 const registrarFacturasController = new RegistrarFacturasController(); 
 
-router.post('/api/registrar-factura', authenticate, isAdminOrContabilidad, registrarFacturasController.crearRegistroFactura);
+router.post('/api/registrar-factura', authenticate, isAdminOrContador, registrarFacturasController.crearRegistroFactura);
 
-router.get('/api/facturas', authenticate, isAdminOrContabilidad, registrarFacturasController.obtenerRegistros);
+router.get('/api/facturas', authenticate, isAdminOrContador, registrarFacturasController.obtenerRegistros);
 
-router.get('/api/facturas-cliente/:id_cliente', authenticate, isAdminOrContabilidad,registrarFacturasController.obtenerRegistroPorCliente);
+router.get('/api/facturas-cliente/:id_cliente', authenticate, isAdminOrContador,registrarFacturasController.obtenerRegistroPorCliente);
 
-router.get('/api/facturas-estado/:estado_factura', authenticate, isAdminOrContabilidad, registrarFacturasController.obtenerPorEstado);
-router.get('/api/facturas-saldo', authenticate, isAdminOrContabilidad, registrarFacturasController.obtenerPorSaldoPendiente);
+router.get('/api/facturas-estado/:estado_factura', authenticate, isAdminOrContador, registrarFacturasController.obtenerPorEstado);
+router.get('/api/facturas-saldo', authenticate, isAdminOrContador, registrarFacturasController.obtenerPorSaldoPendiente);
 
-router.get('/api/factura-numero/:numero_factura', authenticate, isAdminOrContabilidad, registrarFacturasController.obtenerRegistroPorNumero);
-router.get('/api/factura/:id', authenticate, isAdminOrContabilidad, registrarFacturasController.obtenerRegistroPorId);
-router.put('/api/factura/:id', authenticate, isAdminOrContabilidad, registrarFacturasController.actualizarRegistroFactura);
-router.delete('/api/eliminar-factura/:id', authenticate, isAdminOrContabilidad, registrarFacturasController.eliminarRegistroFactura);
+router.get('/api/factura-numero/:numero_factura', authenticate, isAdminOrContador, registrarFacturasController.obtenerRegistroPorNumero);
+router.get('/api/factura/:id', authenticate, isAdminOrContador, registrarFacturasController.obtenerRegistroPorId);
+router.put('/api/factura/:id', authenticate, isAdminOrContador, registrarFacturasController.actualizarRegistroFactura);
+router.delete('/api/eliminar-factura/:id', authenticate, isAdminOrContador, registrarFacturasController.eliminarRegistroFactura);
 
 export default router;
