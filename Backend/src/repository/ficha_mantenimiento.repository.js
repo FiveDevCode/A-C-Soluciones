6e1b29f9a86ecd638a6fcc24afb39e9e7bb4f44c
RobertoCarl32 <castillo.roberto@correounivalle.edu.co>
2025-12-09 19:37:55 -0500

    Ya funcional

--- Backend/src/repository/ficha_mantenimiento.repository.js
+++ Backend/src/repository/ficha_mantenimiento.repository.js
@@ -1,5 +1,9 @@
 import { FichaModel } from '../models/ficha_mantenimiento.model.js';
-
+import { VisitaModel } from '../models/visita.model.js';
+import { SolicitudModel } from '../models/solicitud.model.js';
+import { ClienteModel } from '../models/cliente.model.js';
+import { TecnicoModel } from '../models/tecnico.model.js';
+import { ServicioModel } from '../models/servicios.model.js';
 
 
 export const crearFicha = async (data) => {
@@ -15,13 +19,102 @@ export const actualizarPDFPath = async (id, path) => {
 
 
 
-export const obtenerFichaPorId = async (id) => {
-  return await FichaModel.FichaMantenimiento.findByPk(id);
+export const obtenerTodasFichas = async (id_visitas = null) => {
+  const where = {};
+  if (id_visitas) {
+    where.id_visitas = id_visitas;
+  }
+
+  return await FichaModel.FichaMantenimiento.findAll({
+    where,
+    include: [
+      {
+        model: VisitaModel.Visita,
+        as: 'visita_asociada',
+        required: false, // LEFT JOIN - incluye fichas sin visita
+        attributes: [
+          'id',
+          'fecha_programada',
+          'duracion_estimada',
+          'estado',
+          'notas_previas',
+          'notas_posteriores'
+        ],
+        include: [
+          {
+            model: SolicitudModel.Solicitud,
+            as: 'solicitud_asociada',
+            attributes: ['id', 'descripcion', 'direccion_servicio', 'comentarios'],
+            include: [
+              {
+                model: ClienteModel.Cliente,
+                as: 'cliente_solicitud',
+                attributes: ['id', 'nombre', 'apellido', 'numero_de_cedula']
+              }
+            ]
+          },
+          {
+            model: ServicioModel.Servicio,
+            as: 'servicio',
+            attributes: ['id', 'nombre']
+          }
+        ]
+      },
+      {
+        model: ClienteModel.Cliente,
+        as: 'cliente_ficha',
+        attributes: ['id', 'nombre', 'apellido', 'numero_de_cedula']
+      },
+      {
+        model: TecnicoModel.Tecnico,
+        as: 'tecnico_ficha',
+        attributes: ['id', 'nombre', 'apellido', 'especialidad']
+      }
+    ],
+    order: [['fecha_de_mantenimiento', 'DESC']]
+  });
 };
 
-export const obtenerFichasPorCliente = async (id_cliente) => {
+export const obtenerFichasPorCliente = async (id_cliente, id_visitas = null) => {
+  const where = { id_cliente };
+  if (id_visitas) {
+    where.id_visitas = id_visitas;
+  }
+
   return await FichaModel.FichaMantenimiento.findAll({
-    where: { id_cliente },
+    where,
+    include: [
+      {
+        model: VisitaModel.Visita,
+        as: 'visita_asociada',
+        required: false,
+        attributes: [
+          'id',
+          'fecha_programada',
+          'duracion_estimada',
+          'estado',
+          'notas_previas',
+          'notas_posteriores'
+        ],
+        include: [
+          {
+            model: SolicitudModel.Solicitud,
+            as: 'solicitud_asociada',
+            attributes: ['id', 'descripcion', 'direccion_servicio']
+          },
+          {
+            model: ServicioModel.Servicio,
+            as: 'servicio',
+            attributes: ['id', 'nombre']
+          }
+        ]
+      },
+      {
+        model: TecnicoModel.Tecnico,
+        as: 'tecnico_ficha',
+        attributes: ['id', 'nombre', 'apellido', 'especialidad']
+      }
+    ],
     order: [['fecha_de_mantenimiento', 'DESC']]
   });
 };
@@ -31,21 +124,48 @@ export const obtenerFichasPorTecnico = async (id_tecnico, id_visitas = null) => 
   if (id_visitas) {
     where.id_visitas = id_visitas;
   }
-  
-  return await FichaModel.FichaMantenimiento.findAll({
-    where,
-    order: [['fecha_de_mantenimiento', 'DESC']]
-  });
-}
-
-
-
-export const obtenerTodasFichas = async (id_visitas) => {
-  const where = {};
-  if (id_visitas) where.id_visitas = id_visitas;
 
   return await FichaModel.FichaMantenimiento.findAll({
     where,
+    include: [
+      {
+        model: VisitaModel.Visita,
+        as: 'visita_asociada',
+        required: false,
+        attributes: [
+          'id',
+          'fecha_programada',
+          'duracion_estimada',
+          'estado',
+          'notas_previas',
+          'notas_posteriores'
+        ],
+        include: [
+          {
+            model: SolicitudModel.Solicitud,
+            as: 'solicitud_asociada',
+            attributes: ['id', 'descripcion', 'direccion_servicio'],
+            include: [
+              {
+                model: ClienteModel.Cliente,
+                as: 'cliente_solicitud',
+                attributes: ['id', 'nombre', 'apellido', 'numero_de_cedula']
+              }
+            ]
+          },
+          {
+            model: ServicioModel.Servicio,
+            as: 'servicio',
+            attributes: ['id', 'nombre']
+          }
+        ]
+      },
+      {
+        model: ClienteModel.Cliente,
+        as: 'cliente_ficha',
+        attributes: ['id', 'nombre', 'apellido', 'numero_de_cedula']
+      }
+    ],
     order: [['fecha_de_mantenimiento', 'DESC']]
   });
 };
